d5d2cd4cb19b9dc9e8e9142e44ae7a84e3d5366d
choisunghw <a01036459946@gmail.com>
2023-12-06 01:56:43 +0900

    merge

--- HalliGalli/src/Client.java
+++ HalliGalli/src/Client.java
@@ -1,4 +1,30 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+<<<<<<< Updated upstream
 public class Client {
  //abcd effffff
+=======
+import javax.swing.JFrame;
+
+public class Client extends JFrame implements Runnable, ActionListener{
+	int cards;
+	
+	public Client() {
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+>>>>>>> Stashed changes
 }
