802701e59267beb19cfe1a4017e89d309d9328e0
choisunghw <a01036459946@gmail.com>
2023-12-06 15:59:17 +0900

    client

--- HalliGalli/src/Client.java
+++ HalliGalli/src/Client.java
@@ -1,23 +1,23 @@
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-class SetBack extends JPanel {
+class BackGround extends JPanel {
 	ImageIcon icon = new ImageIcon("images/backG.jpg");
 	Image img = icon.getImage();
 
-	@Override
-	protected void paintComponent(Graphics g) {
+	
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(img, 0, 0, this);
 	}
@@ -25,15 +25,25 @@ class SetBack extends JPanel {
 
 public class Client extends JFrame {
 	int cards;
-	JLabel[] playerLabels = new JLabel[4];
-	JButton[] playerButtons = new JButton[4];
+	JLabel[] playerLabels = new JLabel[4];     //플레이어
+	JButton[] playerButtons = new JButton[4];  //플레이어 버튼
 	Image backgroundImage;
+	
+	
 	
 	public Client() {
 		setTitle("할리갈리");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null); // 절대 위치 배치를 위해 null 레이아웃 사용
+        setLayout(null); 
         
+        BackGround backGround = new BackGround();
+    //	backGround.setOpaque(false); //패널 불투명도 설정
+    	setContentPane(backGround);
+
+    
+    	Container p = getContentPane();
+    	p.setLayout(null);
+    	
         // 플레이어 라벨 생성
         for (int i = 0; i < 4; i++) {
             playerLabels[i] = new JLabel("Player " + (i + 1));
@@ -42,10 +52,10 @@ public class Client extends JFrame {
             playerLabels[i].setSize(100, 50);
 
         }
-    		playerLabels[0].setLocation(50,30);
-    		playerLabels[1].setLocation(450,30);
-    		playerLabels[2].setLocation(50,330);
-    		playerLabels[3].setLocation(450,330);
+    		playerLabels[0].setLocation(320,30);
+    		playerLabels[1].setLocation(670,30);
+    		playerLabels[2].setLocation(320,330);
+    		playerLabels[3].setLocation(670,330);
     		
     	for (int i = 0; i < 4; i++) {
     		add(playerLabels[i]);
@@ -59,14 +69,16 @@ public class Client extends JFrame {
          
            
         }
-         playerButtons[0].setLocation(70,280);
- 		playerButtons[1].setLocation(470,280);
- 		playerButtons[2].setLocation(70,580);
- 		playerButtons[3].setLocation(470,580);
+        playerButtons[0].setLocation(340,280);
+ 		playerButtons[1].setLocation(690,280);
+ 		playerButtons[2].setLocation(340,580);
+ 		playerButtons[3].setLocation(690,580);
  		
  	for (int i = 0; i < 4; i++) {
  		add(playerButtons[i]);
  	}
+ 	
+	
 
         setSize(900, 700);
         setVisible(true);
@@ -77,7 +89,10 @@ public class Client extends JFrame {
 	
 	public static void main(String[] args) {
 		new Client();
-		//System.out.println("??");
+		
+		
+	
+
 		
 	}
 
