31ff988306eca7150958fd12a7bb97bca75ff8cc
se0nghwanchoi <a01036459946@gmail.com>
2023-12-14 01:34:44 +0900

    Card Open버튼, 게임상황패널 추가

--- HalliGalli/src/Client.java
+++ HalliGalli/src/Client.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
@@ -13,22 +14,32 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 
-public class Client extends JFrame {
+public class Client extends JFrame /*implements Runnable, ActionListener*/ {
 	int cards;
 	JLabel[] playerLabels = new JLabel[4];     //플레이어
-	JButton[] playerButtons = new JButton[4];  //플레이어 버튼
+	//JButton[] playerButtons = new JButton[4];  //플레이어 버튼
 	Image backgroundImage;
+	JScrollPane 				scroll_Pane;
 	
-	 ImageIcon bellIcon = new ImageIcon("images/Bell.png");   //종 이미지
-	 ImageIcon bellRIcon = new ImageIcon("images/BellR.png"); //종이 눌렸을때 이미지
+	JTextArea 					text_Area;
+	JTextField 					text_Field; 
+	
+	ImageIcon bellIcon = new ImageIcon("images/Bell.png");   //종 이미지
+	ImageIcon bellRIcon = new ImageIcon("images/BellR.png"); //종이 눌렸을때 이미지
+	ImageIcon cardOpenIcon = new ImageIcon("images/card_open.png");    //카드 오픈 버튼 이미지
+	ImageIcon cardOpenPIcon = new ImageIcon("images/card_open_p.png");  //카드 오픈 버튼이 눌렸을때
 
-	 JButton bellButton = new JButton(bellIcon); 
+	
+	JButton bellButton = new JButton(bellIcon); 
 	 
 	    // bellButton을 위한 ActionListener 구현
-	 ActionListener bellActionListener = new ActionListener() {
+	ActionListener bellActionListener = new ActionListener() {
 	        @Override
 	        public void actionPerformed(ActionEvent e) {
 	            bellButton.setIcon(bellRIcon); // 이미지 변경
@@ -91,32 +102,84 @@ public class Client extends JFrame {
     		add(playerLabels[i]);
     	}
 
-         //플레이어 버튼 생성 
-        for (int i = 0; i < 4; i++) {
-            playerButtons[i] = new JButton("Card Open");
-            playerButtons[i].setFont(new Font("Serif", Font.BOLD, 10)); 
-            playerButtons[i].setSize(100, 25);
-         
-           
-        }
-        playerButtons[0].setLocation(340,280);
- 		playerButtons[1].setLocation(690,280);
- 		playerButtons[2].setLocation(340,580);
- 		playerButtons[3].setLocation(690,580);
- 		
- 		for (int i = 0; i < 4; i++) {
- 		add(playerButtons[i]);
- 		}
  	
  		
  		//bell 버튼 설정
  		bellButton.setSize(bellIcon.getIconWidth(), bellIcon.getIconHeight());
-        bellButton.setLocation(500, 250);
+        bellButton.setLocation(480, 240);
         bellButton.addActionListener(bellActionListener); // ActionListener 추가
         bellButton.setOpaque(false); // 배경색을 투명으로 설정
         bellButton.setContentAreaFilled(false); // 내용 영역의 채우기를 비활성화하여 투명하게 설정
         bellButton.setBorderPainted(false); // 테두리를 없애서 투명하게 설정
         add(bellButton);
+        
+        
+        //채팅창 설정        
+        JPanel ChatPanel = new JPanel();
+		ChatPanel.setLayout(new BorderLayout());
+		ChatPanel.setLocation(30, 200);
+		ChatPanel.setSize(229, 300);
+	
+		text_Area = new JTextArea("채팅창", 1, 1);
+		scroll_Pane = new JScrollPane(text_Area);
+		text_Area.setEditable(false);
+		text_Field = new JTextField("");
+		text_Field.setFont(new Font("Serif", Font.BOLD, 12));
+		text_Field.setSize(229, 40);
+
+		ChatPanel.add(scroll_Pane, BorderLayout.CENTER);
+		ChatPanel.add(text_Field, BorderLayout.SOUTH);
+		p.add(ChatPanel);
+		
+		 // 카드 뒤집기 버튼 추가
+		JButton flipCardButton = new JButton(cardOpenIcon);
+		flipCardButton.setSize(cardOpenIcon.getIconWidth(), cardOpenIcon.getIconHeight());
+		flipCardButton.setLocation(30, 520);
+		flipCardButton.setOpaque(false);
+		flipCardButton.setContentAreaFilled(false);
+		flipCardButton.setBorderPainted(false);
+		flipCardButton.setBorder(null);
+		flipCardButton.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        flipCardButton.setIcon(cardOpenPIcon); // 이미지 변경
+		       
+		        
+		        // javax.swing.Timer 사용
+		        javax.swing.Timer timer = new javax.swing.Timer(1000, new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent e) {
+		                flipCardButton.setIcon(cardOpenIcon); // 1초 후에 원래 이미지로 변경
+		                
+		            }
+		        });
+
+		        timer.setRepeats(false); // 타이머를 한 번만 실행하도록 설정
+		        timer.start(); // 타이머 시작
+		    }
+		});
+		p.add(flipCardButton);
+		
+		
+		// 채팅창 상단에 게임 상황을 표시하는 패널 추가
+		JPanel gameStatusPanel = new JPanel();
+		//gameStatusPanel.setLayout(new BorderLayout());
+		gameStatusPanel.setLocation(30, 110); // 상단에 배치할 예정이므로 적절한 위치 지정
+		gameStatusPanel.setSize(229, 110); // 남은 공간만큼의 크기 설정
+		// 예시로 JLabel을 사용하여 간단한 게임 상황 표시
+		JLabel gameStatusLabel = new JLabel("다른 플레이어를 기다리는중..");
+		gameStatusLabel.setFont(new Font("Serif", Font.BOLD, 16));
+		gameStatusLabel.setForeground(Color.BLACK); 
+		gameStatusPanel.add(gameStatusLabel);
+		gameStatusPanel.setOpaque(false); // 배경을 투명하게 설정하여 채팅 창의 배경 이미지가 보이도록 함
+
+		// 메인 패널에 게임 상황 패널 추가
+		p.add(gameStatusPanel);
+		
+		
+		
+		
+		
     
         setSize(900, 700);
         setVisible(true);
@@ -127,10 +190,7 @@ public class Client extends JFrame {
 	
 	public static void main(String[] args) {
 		new Client();
-		
-		
 	
-
 		
 	}
 }
